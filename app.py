d6345c94ebd97becfe230ff5fca0ca6c9206fdf0
Francisco Carlos <93539795+FranciscoCarlos1@users.noreply.github.com>
2025-01-06 11:44:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def processar_indicacao():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="84.32.84.32", port=80)
